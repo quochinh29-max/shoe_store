c29c74e44d515ab083cd4757030e1301f83a3376
quochinh29-max <quochinh29@gmail.com>
2026-07-23 14:54:57 +0700

    Initial commit

---
+++ src/main/java/com/example/quanlybangiay/QuanlybangiayApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlybangiay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlybangiayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlybangiayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quanlybangiay/QuanlybangiayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlybangiay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlybangiayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
